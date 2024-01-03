2063b6ca4a18f8a82ee04b6e948f0410bce1819c
Jiro Matsuzawa <matsuzawa.jr@gmail.com>
2024-01-03 16:37:13 +0900

    sh: Implement has_command func

--- .config/sh/env.sh
+++ .config/sh/env.sh
@@ -10,6 +10,10 @@ else
   export ENV_SH_INCLUDED
 fi
 
+has_command() {
+  type "$1" > /dev/null 2>&1
+}
+
 if [ -z "${XDG_CONFIG_HOME}" ]; then
   XDG_CONFIG_HOME="${HOME}/.config"
   export XDG_CONFIG_HOME
